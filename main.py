da0b13183b56f13bff26c69b7ca7f9d708da21bc
Prateek Pani <prateek.pani4243@gmail.com>
2020-12-02 15:42:53 +0530

    sanity check on correct read + write from/to json

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import json
 
 
 paths_of_images = glob('static/mnist/*.jpg')
+glob_idx = [i for i in range(len(paths_of_images))]
 # paths_of_images = glob('static/f_le/*.png')
 req_dict = {le:'-1' for le in paths_of_images}
 # print(type(paths_of_images)) #list
@@ -19,13 +20,15 @@ unseen_idx_list = list(unseen_idx_set)
 
 class_of_all_images = [-1]*len(paths_of_images) # stores the class annotations of all the images by initialising -1.
 indices_of_displayed = list(range(24)) # stores indices of currently displayed images
+
+# Here read from file logic for every new session
 with open('mnist_data.json','r') as f:
 # with open('fundus_data.json','r') as f:
     m = json.loads(f.read())
     for i in range(len(paths_of_images)):
         class_of_all_images[i] = m[paths_of_images[i].split("/")[-1]]
-        # print(m[paths_of_images[i].split("/")[-1]]) 
 
+# print(class_of_all_images) #states of all the images uptil this point.
 
 def card_body(card_id):
     return [
@@ -84,6 +87,11 @@ def gen_cards(batch):
     Input("next", "n_clicks")
 )
 def next(c1):
+    # if it's check_point 0
+    # else if it's not predict next set of checkpoints and initialize to radiobuttons
+    # For this understand working of gen_cards, card, card_body, 
+    global class_of_all_images
+    print(class_of_all_images)
     global indices_of_displayed
     global unseen_idx_set
     batch = list(unseen_idx_set)[:24]    
@@ -97,13 +105,15 @@ def next(c1):
     Input("save", "n_clicks")
 )
 def save(c1):
+    global class_of_all_images
     m = {}
     print(c1)
     for i in range(len(class_of_all_images)):
         m[paths_of_images[i].split("/")[-1]] = class_of_all_images[i]
     
+    print(f"during save: {class_of_all_images}")
     # with open('fundus_data.json', "w+") as f:
-    with open('mnist_data.json', "w+") as f:
+    with open('mnist_data.json', "w") as f:
         f.write(json.dumps(m))
     print(c1)
     return ""
@@ -114,9 +124,13 @@ def save(c1):
     Input({'type': 'label-option','index': MATCH}, 'id')
 )
 def button_click(value, id):
-
+    # I ahve the value of the recording here , 
+    # Manipulate here and store in a datastructure and fire when save is clicked
     # print(f"val: {value}, id: {id}")
+    global glob_idx
+    global req_dict
     class_of_all_images[int(id['index'])] = int(value)
+    # req_dict[ int(id['index'] 
     print(f"class of {id['index']} set to {value}")
     # print(class_of_all_images)
     print(id)
@@ -124,4 +138,4 @@ def button_click(value, id):
 
 
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=3320 ,debug=True)
+    app.run_server(host='127.0.0.1', port=2320 ,debug=True)
