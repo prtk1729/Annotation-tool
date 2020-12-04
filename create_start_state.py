9203f291042bb7b3b3a4ace221372ce94d041674
prtk1729 <prateek.pani4243@gmail.com>
2020-12-04 06:46:07 +0530

    file to reset the json file after save button click

--- create_start_state.py
+++ create_start_state.py
@@ -1,8 +1,11 @@
 import json  
 from glob import glob
 
-# paths_of_images = glob('static/f_le/*.png')
-paths_of_images = glob('static/mnist/*.jpg')
-req_dict = {le[13:]:'-1' for le in paths_of_images}
-with open("mnist_data.json", "w") as outfile:  
-    json.dump(req_dict, outfile) 
+def reset_json_file():
+    # paths_of_images = glob('static/f_le/*.png')
+    paths_of_images = glob('static/mnist/*.jpg')
+    req_dict = {le[13:]:'-1' for le in paths_of_images}
+    with open("mnist_data.json", "w") as outfile:  
+        json.dump(req_dict, outfile) 
+    print('\nSuccessfully reset the json file!! Can Start parsing again from scratch\n')
+    return
