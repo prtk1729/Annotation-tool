9599d576b7939e35809962a47f41115b52277c3d
prtk1729 <prateek.pani4243@gmail.com>
2020-12-04 06:42:06 +0530

    python file to check consistency after every save click

--- check_consistency.py
+++ check_consistency.py
@@ -2,9 +2,9 @@ import json
 from glob import glob  
 
 paths_of_images = glob('static/mnist/*.jpg')
-image_key_72 = str(paths_of_images[72][13:])
-image_key_73 = str(paths_of_images[73][13:])
-image_key_74 = str(paths_of_images[74][13:])
+image_key_72 = str(paths_of_images[24][13:])
+image_key_73 = str(paths_of_images[25][13:])
+image_key_74 = str(paths_of_images[26][13:])
 
 with open('mnist_data.json','r') as f:
     m = json.load(f)
