d3cb6d46d6937c31545d160cc62d9dc43b2f7829
prtk1729 <prateek.pani4243@gmail.com>
2020-12-04 07:24:54 +0530

    last_check_point logic + most_confused_24 blackbox + predict_next_24_states + RadioButtons placeholders for next_24 states
    incorporated last_checkpont logic to handle bugs regarding 1st and last iteration numbers; added predict_next_24_states() as a blackbox for now invoked when next button is clicked; added most_confused() as a black box for now invoked when next button is clicked

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from glob import glob
 import json
 ## Global variables 
 
+from create_start_state import reset_json_file
+
+unseen_idx_set = set({})
+unseen_idx_list = []
 
 def check_point():
     '''function reads the last_checkpoint file and returns the current iter_no'''
@@ -17,7 +21,11 @@ def check_point():
     lc.close()
     iter_no = int(iter_no)
     return iter_no
+
 iter_no = check_point()
+print(f'Resuming from iteration: {iter_no}')
+
+
 
 
 paths_of_images = glob('static/mnist/*.jpg')
@@ -33,8 +41,7 @@ with open('mnist_data.json','r') as f:
         class_of_all_images[i] = m[paths_of_images[i].split("/")[-1]]
 # print(class_of_all_images) #states of all the images uptil this point.
 
-unseen_idx_set = set({})
-unseen_idx_list = []
+
 
 req_dict = {le:'-1' for le in paths_of_images} #for this load dict
 
@@ -44,6 +51,7 @@ req_dict = {le:'-1' for le in paths_of_images} #for this load dict
 my_file = open("./your_file.txt", "r")
 content = my_file.read()
 if len(list(content)) == 0:
+    # handle case when iter_no != 0 and len(list(content)) == 0: inconsistent case return with prompt.
     unseen_idx_set = set([i for i in range(len(paths_of_images))])
     unseen_idx_list = list(unseen_idx_set)
 else:
@@ -56,17 +64,15 @@ else:
 
 
 
-def predict_next_24_states():
-    '''invoked from next i.e when "next_btn" is clicked and Uses ML to predict
-    placeholders for next set of 24 points.....for now placeholders follow the following logic'''
-    pass
-
-def most_confused_24():
-    '''calculates most confused 24 images based on HITL....
-    for now random 24 from unseen_idx_set'''
-    pass
-
 def card_body(card_id):
+    global current_24
+    global state_24
+    global paths_of_images
+
+    for i in range(len(current_24)):
+        if current_24[i]==card_id:
+            break
+    # print(f"inside card_body with current_24[i]: {current_24[i]}")
     return [
         dbc.CardImg(src=paths_of_images[card_id], top=True, style={"height":"150px", "width": "180px"}),
         dbc.CardBody([                
@@ -81,7 +87,7 @@ def card_body(card_id):
                     {'label': 'Grade 2', 'value': f"2"},
                     {'label': 'Grade 3', 'value': f"3"}
                 ],
-                value=str(class_of_all_images[card_id]),
+                value=str(state_24[i]),
                 labelStyle={'display': 'inline-block', "padding": "0px 5px 0px 0px", "margin-bottom": "0px"},
                 inputStyle = {"margin-right": "2px"},
                 className=""
@@ -90,6 +96,9 @@ def card_body(card_id):
         ]
 
 def card(card_id):
+    # print('hi')
+    global current_24
+    # print(f"inside card(): {current_24[int(i)]}")
     title = "title"
     description = "desc"
     return dbc.Card(card_body(card_id), id={
@@ -109,6 +118,9 @@ app.layout = html.Div([
 ], style={'text-align': "center", "margin": "1em 0em"})
 
 def gen_cards(current_24):
+    print(f"inside gen_cards with current_24: {current_24}")
+    # l = [i+16 for i in range(len(current_24[:8])) ]
+    # print(l)
     return [
         dbc.Row([
             dbc.Col([card(i)]) for i in current_24[:8]], justify="start"),
@@ -118,6 +130,24 @@ def gen_cards(current_24):
             dbc.Col([card(i)]) for i in current_24[16:24]], justify="end"),
         ]
 
+def predict_next_24_states(next_24):
+    '''invoked from next i.e when "next_btn" is clicked and Uses ML to predict
+    placeholders for next set of 24 points.....for now placeholders follow the following logic'''
+    global state_24
+
+    state_24 = [le%4 for le in next_24]
+    # print(f"inside predict_next_24_states() with state_24: {state_24}")
+    return state_24
+
+def most_confused_24():
+    '''invoked from next ; calculates most confused 24 images based on HITL....
+    for now random 24 from unseen_idx_set'''
+    global unseen_idx_set
+    next_24 = list(unseen_idx_set)[:24]
+    # print(f"inside most_confused() with next_24: {next_24}")
+    return next_24
+
+
 @app.callback(
     Output('card-deck', 'children'),
     Input("next", "n_clicks")
@@ -126,12 +156,31 @@ def next(c1):
     '''calculates next set of 24 indices and assigns placeholders to these before loading
     invoke most_confused_24 and then predict_next_24_states'''
     global class_of_all_images
-    # print(class_of_all_images)
-    global indices_of_displayed
     global unseen_idx_set
+    global current_24
+    global state_24
+    global iter_no
+
+    # check iter_no
+    if int(iter_no) >= (len(paths_of_images)//24 + 1):
+        file1 = open("./last_checkpoint.txt","w") 
+        file1.write('0')
+        file1.close()
+
+        # before reseting mnist_json/fundus_json files 1st copy the recordings to another file
+        with open("mnist_data.json", "r") as f1, open("previous_recordings.json", "w") as f2:
+            f2.write(f1.read())
+        reset_json_file()
+        print("\n\n\n ======= No more images to be parsed. Needs a Hard reset to iterate from scratch again\n\n\n")
+
+        return
 
     # no need to read from your_file.txt
-    current_24 = list(unseen_idx_set)[:24]    
+    current_24 = most_confused_24() #next_24 will be current_24 for next iter
+    # print(f"inside next() with current_24: {current_24}")
+    state_24 = predict_next_24_states(current_24)   #returned state list of these current_24 points 
+    # print(f"inside next() with state_24: {state_24}")
+
     return gen_cards(current_24)
 
 
@@ -145,7 +194,6 @@ def save(c1):
 
     global iter_no
     global class_of_all_images
-    global indices_of_displayed
     global unseen_idx_set
     current_24 = list(unseen_idx_set)[:24]    
 
@@ -198,4 +246,4 @@ def button_click(value, id):
 
 
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=2490 ,debug=True)
+    app.run_server(host='127.0.0.1', port=6590 ,debug=True)
